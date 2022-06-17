54beb7501acc828fe8fffb36f6cbb59f3582fa32
ScolzeWA <64407840+ScolzeWA@users.noreply.github.com>
2022-06-17 23:40:21 +0200

    Add files via upload

--- program/help.py
+++ program/help.py
@@ -1,6 +1,6 @@
 import asyncio
 from driver.filters import command
-from config import BOT_NAME, SUPPORT_GROUP, START_IMG
+from config import BOT_NAME, GROUP_SUPPORT, BOT_PHOTO
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
@@ -9,7 +9,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 @Client.on_message(command("help") & filters.private & ~filters.group & ~filters.edited)
 async def help_cmd(client: Client, message: Message):
     await message.reply_sticker("CAACAgUAAx0CZIiVngABAoCAYqWU-JzBZtfz14vr_DfDkJyy7X8AAjYGAAIsk1lUo7RMhQfOm28eBA")
-    await message.reply_photo(f"{START_IMG}", caption=f"""
+    await message.reply_photo(f"{BOT_PHOTO}", caption=f"""
 ❄ **ᴀᴠᴀɪʟᴀʙʟᴇ ᴄᴏᴍᴍᴀɴᴅs ɪɴ {BOT_NAME} :**
 
 ๏ /play : sᴛᴀʀᴛs sᴛʀᴇᴀᴍɪɴɢ ᴛʜᴇ ʀᴇǫᴜᴇsᴛᴇᴅ ᴛʀᴀᴄᴋ ᴏɴ ᴠɪᴅᴇᴏᴄʜᴀᴛ.
