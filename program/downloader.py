44827f39781f6b40a6d0e06ab37bedf9f4945ab1
ScolzeWA <64407840+ScolzeWA@users.noreply.github.com>
2022-06-17 04:50:46 +0200

    Update downloader.py

--- program/downloader.py
+++ program/downloader.py
@@ -85,57 +85,7 @@ def song(_, message):
         os.remove(thumb_name)
     except Exception as e:
         print(e)
-
-@Client.on_message(command2(["تحميل","تحميل_موسيقي"]) & ~filters.edited)
-def song(_, message):
-    query = " ".join(message.command[1:])
-    m = message.reply("🔎 جاري البحث انتظر قليلآ...")
-    ydl_ops = {"format": "bestaudio[ext=m4a]"}
-    try:
-        results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://youtube.com{results[0][ url_suffix ]}"
-        title = results[0]["title"][:40]
-        thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"{title}.jpg"
-        thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
-        duration = results[0]["duration"]
-
-    except Exception as e:
-        m.edit("❌ لم يتم العثور على الاغنية\n\nيرجى إعطاء اسم أغنية صالح")
-        print(str(e))
-        return
-    m.edit("📥 جاري تحميل الملف...")
-    try:
-        with yt_dlp.YoutubeDL(ydl_ops) as ydl:
-            info_dict = ydl.extract_info(link, download=False)
-            audio_file = ydl.prepare_filename(info_dict)
-            ydl.process_info(info_dict)
-        rep = f"**🎧 الرافع @{bn}**"
-        secmul, dur, dur_arr = 1, 0, duration.split(":")
-        for i in range(len(dur_arr) - 1, -1, -1):
-            dur += int(float(dur_arr[i])) * secmul
-            secmul *= 60
-        m.edit("📤 جاري تحميل الملف...")
-        message.reply_audio(
-            audio_file,
-            caption=rep,
-            thumb=thumb_name,
-            parse_mode="md",
-            title=title,
-            duration=dur,
-        )
-        m.delete()
-    except Exception as e:
-        m.edit("❌ خطأ")
-        print(e)
-
-    try:
-        os.remove(audio_file)
-        os.remove(thumb_name)
-    except Exception as e:
-        print(e)
-
+        
 @Client.on_message(
     command(["vsong","vsong@{bn}","video"]) & ~filters.edited
 )
@@ -184,58 +134,8 @@ async def vsong(client, message):
         await msg.delete()
     except Exception as e:
         print(e)
-        
-@Client.on_message(
-    command2(["تحميل_فيديو","تحميل فيديو"]) & ~filters.edited
-)
-async def vsong(client, message):
-    await message.delete()
-    ydl_opts = {
-        "format": "best",
-        "keepvideo": True,
-        "prefer_ffmpeg": False,
-        "geo_bypass": True,
-        "outtmpl": "%(title)s.%(ext)s",
-        "quite": True,
-    }
-    query = " ".join(message.command[1:])
-    try:
-        results = YoutubeSearch(query, max_results=1).to_dict()
-        link = f"https://youtube.com{results[0][ url_suffix ]}"
-        title = results[0]["title"][:40]
-        thumbnail = results[0]["thumbnails"][0]
-        thumb_name = f"{title}.jpg"
-        thumb = requests.get(thumbnail, allow_redirects=True)
-        open(thumb_name, "wb").write(thumb.content)
-        results[0]["duration"]
-        results[0]["url_suffix"]
-        results[0]["views"]
-        message.from_user.mention
-    except Exception as e:
-        print(e)
-    try:
-        msg = await message.reply("📥 **جاري تحميل الفيديو...**")
-        with YoutubeDL(ydl_opts) as ytdl:
-            ytdl_data = ytdl.extract_info(link, download=True)
-            file_name = ytdl.prepare_filename(ytdl_data)
-    except Exception as e:
-        return await msg.edit(f"🚫 **خطأ:** {e}")
-    preview = wget.download(thumbnail)
-    await msg.edit("📤 **جاري تحميل الفيديو...**")
-    await message.reply_video(
-        file_name,
-        duration=int(ytdl_data["duration"]),
-        thumb=preview,
-        caption=ytdl_data["title"],
-    )
-    try:
-        os.remove(file_name)
-        await msg.delete()
-    except Exception as e:
-        print(e)
 
-
-@Client.on_message(command(["lyric", f"بحث"]))
+@Client.on_message(command(["lyric"]))
 async def lyrics(_, message):
     try:
         if len(message.command) < 2:
@@ -250,20 +150,4 @@ async def lyrics(_, message):
         await rep.edit(result)
     except Exception:
         await rep.edit("❌ **لم يتم العثور على نتائج كلمات غنائية**\n\n» **يرجى إعطاء اسم أغنية صالح**")
-        
-@Client.on_message(command2(["بحث"]))
-async def lyrics(_, message):
-    await message.delete()
-    try:
-        if len(message.command) < 2:
-            await message.reply_text("» **قم باارسال اسم المقطع**")
-            return
-        query = message.text.split(None, 1)[1]
-        rep = await message.reply_text("🔎 **جاري البحث عن كلمات...**")
-        resp = requests.get(
-            f"https://api-tede.herokuapp.com/api/lirik?l={query}"
-        ).json()
-        result = f"{resp[ data ]}"
-        await rep.edit(result)
-    except Exception:
-        await rep.edit("❌ **لم يتم العثور على نتائج كلمات غنائية**\n\n» **يرجى إعطاء اسم أغنية صالح**")
+       
